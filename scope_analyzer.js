cf95afa2560c893860d729b718d200bf54cfb5a2
Lennart Kats <lclkats@gmail.com>
2013-09-10 10:52:32 +0200

    Avoid completing on /

--- scope_analyzer.js
+++ scope_analyzer.js
@@ -395,7 +395,7 @@ handler.getIdentifierRegex = function() {
 
 handler.complete = function(doc, fullAst, pos, currentNode, callback) {
     var line = doc.getLine(pos.row);
-    var identifier = completeUtil.retrievePrecedingIdentifier(line, pos.column);
+    var identifier = completeUtil.retrievePrecedingIdentifier(line, pos.column, handler.getIdentifierRegex());
 
     var matches = completeUtil.findCompletions(identifier, SCOPE_ARRAY);
     callback(matches.map(function(m) {
