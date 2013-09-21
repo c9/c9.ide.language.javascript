6ce5564fe707f8d7060348cff829d75119a929a3
Ruben Daniels <ruben@c9.io>
2013-09-21 14:17:38 +0000

    Fix for language

--- JSResolver.js
+++ JSResolver.js
@@ -44,7 +44,7 @@ define(function(require, exports, module) {
         var preview = "<b>Add semicolon</b><p>" + before + "<b>; </b>" + after + "</p>";
         
         var insert = ";";
-        if (afterLength){
+        if (after.length){
             insert += " ";
         }
 
