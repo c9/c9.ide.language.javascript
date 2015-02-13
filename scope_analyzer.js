d21ed53af3493d5d1deed5c80f3b133bd8744fe5
Lennart kats <lennart@c9.io>
2015-02-13 14:54:21 +0000

    Disable spurious warning

--- scope_analyzer.js
+++ scope_analyzer.js
@@ -590,7 +590,8 @@ handler.analyze = function(value, ast, callback, minimalAnalysis) {
                 'If(Var("err"), Call(fn, args), None())', function(b, node) {
                     // Check if the `err` variable is used somewhere in the function arguments.
                     if (b.args.collectTopDown('Var("err")').length > 0 &&
-                        !b.fn.isMatch('PropAccess(Var("console"), _)'))
+                        !b.fn.isMatch('PropAccess(Var("console"), _)') &&
+                        !b.fn.isMatch('PropAccess(_, "log")'))
                         markers.push({
                             pos: b.fn.getPos(),
                             type: 'warning',
